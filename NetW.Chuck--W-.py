4baa38b09bc64ada1b9bb45667dce475ddcdadff
Emry-McLaurine <74030146+Emry-McLaurine@users.noreply.github.com>
2023-01-11 10:41:40 -0500

    Add files via upload

---
+++ NetW.Chuck--W-.py
@@ -0,0 +1,16 @@
+# Write your code here :-)
+# food people makede
+
+print("Hello, welcome to food place!!!")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", thank you for coming in today")
+
+print("what can I get for you " + name)
+
+print("We have water, ice, and salt")
+
+ordered = input()
+
+print(" Your " + ordered + ", will be ready soon!!")
